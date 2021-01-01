f244ff76b97b0168fb81ff9b452991ebd2329fc4
Yuto Takamoto <scarletjaeger@gmail.com>
2021-01-01 06:07:56 +0000

    Create gcr repo before iam

--- tf/project.tf
+++ tf/project.tf
@@ -15,7 +15,7 @@ resource "google_project_service" "enabled-apis" {
 
   # http://amat.su/Rb8gSP
   for_each = toset([
-    "artifactregistry.googleapis.com",
+    "containerregistry.googleapis.com",
     "iam.googleapis.com",
     "sqladmin.googleapis.com",
     "compute.googleapis.com",
