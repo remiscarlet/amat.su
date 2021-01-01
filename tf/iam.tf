94106ed67e52d629ae1b38184227a04cbfdbe9e0
Yuto Takamoto <scarletjaeger@gmail.com>
2021-01-01 06:02:12 +0000

    GAR -> GCR

--- tf/iam.tf
+++ tf/iam.tf
@@ -6,11 +6,24 @@ resource "google_service_account" "gcr_uploader" {
   display_name = "Amatsu GCR Uploader"
 }
 
-resource "google_artifact_registry_repository_iam_member" "test-iam" {
-  provider = google-beta
+#resource "google_artifact_registry_repository_iam_member" "test-iam" {
+#  provider = google-beta
+#
+#  location   = google_artifact_registry_repository.amatsu.location
+#  repository = google_artifact_registry_repository.amatsu.name
+#  role       = "roles/artifactregistry.writer"
+#  member     = "serviceAccount:${google_service_account.gcr_uploader.email}"
+#}
 
-  location   = google_artifact_registry_repository.amatsu.location
-  repository = google_artifact_registry_repository.amatsu.name
-  role       = "roles/artifactregistry.writer"
-  member     = "serviceAccount:${google_service_account.gcr_uploader.email}"
+resource "google_container_registry" "amatsu-images" {
+  project = var.project_id
+}
+
+resource "google_storage_bucket_iam_binding" "gcr-docker-image-access" {
+  bucket = "artifacts.${var.project_id}.appspot.com"
+  role   = "roles/storage.objectCreator"
+
+  members = [
+    "serviceAccount:${google_service_account.yc-infra-bot.email}"
+  ]
 }
